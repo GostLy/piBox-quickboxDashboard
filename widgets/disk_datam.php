b526bc75777736ab0190382b802ec3f09bf85a53
GostLy <gostlyis@gmail.com>
2019-10-30 19:09:12 -0400

    Update disk_datam.php

--- widgets/disk_datam.php
+++ widgets/disk_datam.php
@@ -89,11 +89,11 @@ if (file_exists('/install/.quota.lock')) {
   echo "mntPath: ".$mntPath."<br>Gbytes: ".$Gbytes."<br>";
   
   $dftotal  = number_format(round(@disk_total_space("$mntPath")/($Gbytes),3)); //Total
-  echo "disk total: ".disk_total_space($mntPath)."<br>";
+  echo "disk total: ".disk_total_space("/mnt/piStorage")."<br>";
   $dffree   = number_format(round(@disk_free_space($mntPath)/($Gbytes),3)); //Available
-  echo "disk free: ".disk_free_space($mntPath)."<br>";
+  echo "disk free: ".disk_free_space("/mnt/piStorage")."<br>";
   $dfused   = number_format(round(@disk_total_space($mntPath)/($Gbytes),3)-round(@disk_free_space($mntPath)/($Gbytes),3)); //used
-  echo "disk used: ".disk_free_space($mntPath) - disk_total_space($mntPath)."<br>";
+  echo "disk used: ".disk_free_space("/mnt/piStorage") - disk_total_space("/mnt/piStorage")."<br>";
   //$dftotal  = @disk_total_space("/mnt/piStorage");
   //$dffree   = @disk_free_space("/mnt/piStorage");
   //$dfused   = round($dffree - $dftotal);
